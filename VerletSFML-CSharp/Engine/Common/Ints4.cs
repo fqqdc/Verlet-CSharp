dd2f3b8454abafe2c8703007c15493d93b7768b8
Emanon <fqqdc@qq.com>
2023-09-28 01:56:26 +0800

    Update Ints4.cs

--- VerletSFML-CSharp/Engine/Common/Ints4.cs
+++ VerletSFML-CSharp/Engine/Common/Ints4.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,20 @@ namespace VerletSFML_CSharp.Engine.Common
         public int int_1;
         public int int_2;
         public int int_3;
+
+        public static ref int Item(ref Ints4 ints4, int index)
+        {
+            var spanInts4 = new Span<Ints4>(ref ints4);
+            return ref MemoryMarshal.Cast<Ints4, int>(spanInts4)[index];
+        }
     }
 
     public static class Ints4Helper
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref int Item(this ref Ints4 ints4, int index)
         {
-            var spanInts4 = new Span<Ints4>(ref ints4);
-            return ref MemoryMarshal.Cast<Ints4, int>(spanInts4)[index];
+            return ref Ints4.Item(ref ints4, index);
         }
     }
 }
